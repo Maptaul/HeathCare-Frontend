510c92c27457f983119fb0b7e95878c03993f1c7
Maptaul <maptaulislam1@gmail.com>
2026-09-21 12:36:24 +0600

    feat: add login form component with input fields and GitHub login option
    - Created a new LoginForm component for user authentication.
    - Implemented reusable Field, Input, Label, and Separator components for form structure.
    - Added styling and accessibility features to input fields and labels.
    - Included a GitHub login button and a link for password recovery.

--- src/components/layout/public/Header.tsx
+++ src/components/layout/public/Header.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Header() {
@@ -13,13 +14,25 @@ export default function Header() {
   ];
   return (
     <header className="w-full h-16 bg-white shadow-md border-b flex items-center justify-center">
-      <nav className="flex items-center">
-        {routes.map((route) => (
-          <Link key={route.name} href={route.url} className="mx-4">
-            {route.name}
-          </Link>
-        ))}
-      </nav>
+      <div className=" flex justify-between items-center w-full max-w-6xl px-4">
+        <div> HealthCare</div>
+        <nav className="flex items-center">
+          {routes.map((route) => (
+            <Link key={route.name} href={route.url} className="mx-4">
+              {route.name}
+            </Link>
+          ))}
+        </nav>
+        <div>
+          <Button
+            variant="outline"
+            render={<Link href="/login">Login</Link>}
+            nativeButton={false}
+          >
+            login
+          </Button>
+        </div>
+      </div>
     </header>
   );
 }
